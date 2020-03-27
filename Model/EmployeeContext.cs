c9b93bd7696c0c4c01b62127ba99ee881ededfb9
Bidesi123 <bidesh.sahoo.1@gmail.com>
2020-03-27 22:30:46 +0530

    [bidesi]Add.EmployeeModel project

---
+++ Model/EmployeeContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+   public class EmployeeContext :DbContext
+    {
+        /*public EmployeeContext(DbContextOptions<EmployeeContext> option) : base(option)
+        {
+
+        }*/
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
