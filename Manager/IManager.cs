3a35be9a472bc8af10179639961783abc2b9a7b6
Bidesi123 <bidesh.sahoo.1@gmail.com>
2020-03-28 09:52:03 +0530

    [bidesi]Add.Manager Project directory

---
+++ Manager/IManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Model;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+  public interface IManager
+    {
+       public Task<int> AddEmployee(Employee employee);
+        public Task<int> DeleteEmployee(Employee employee);
+        public IEnumerable<Task<int>> GetAllEmployee();
+
+    }
+}
---
+++ Manager/ImpManager.cs
@@ -0,0 +1,31 @@
+﻿using Model;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    class ImpManager : IManager
+    {
+        private readonly IRepository repo;
+        public ImpManager(IRepository repo)
+        {
+            this.repo = repo;
+        }
+        public Task<int> AddEmployee(Employee employee)
+        {
+            return this.repo.AddEmployee(employee);
+        }
+        public Task<int> DeleteEmployee(Employee employee)
+        {
+            return this.repo.DeleteEmployee(employee);
+        }     
+        public IEnumerable<Task<int>> GetAllEmployee()
+        {
+            return this.repo.GetAllEmployee();
+        }
+
+    }
+}
