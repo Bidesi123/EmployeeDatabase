f515984a33303d49944e9fb1f5a4df39c489ce32
Bidesi123 <bidesh.sahoo.1@gmail.com>
2020-03-28 09:53:14 +0530

    [bidesi]Add.Repository project

---
+++ Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Model;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+   public interface IRepository
+    {
+        public Task<int> GetEmployee(int id);
+       public IEnumerable<Task<int>> GetAllEmployee();
+
+        public Task<int> AddEmployee(Employee employee);
+        public Task<int> UpdateEmployee(Employee employee);
+        public Task<int> DeleteEmployee(Employee employee);
+        
+    }
+}
---
+++ Repository/ImpRepository.cs
@@ -0,0 +1,43 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ImpRepository : IRepository
+    {
+        private readonly EmployeeContext employeeContext;
+        public ImpRepository(EmployeeContext employeeContext)
+        {
+            this.employeeContext = employeeContext;
+        }
+        public Task<int> GetEmployee(int id)
+        {
+            EmployeeContext.Employees.Find(id);
+
+        }
+        public Task<int> AddEmployee(Employee employee)
+        {
+           employeeContext.Employees.Add(employee);
+           var result= employeeContext.SaveChangesAsync();
+           return result;           
+        }
+        public Task<int> UpdateEmployee(Employee employee)
+        {
+            
+        }
+        public Task<int> DeleteEmployee(Employee employee)
+        {
+            employeeContext.Employees.Remove(employee);
+            var result = employeeContext.SaveChangesAsync();
+            return result;
+        }
+
+        IEnumerable<Task<int>> IRepository.GetAllEmployee()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
